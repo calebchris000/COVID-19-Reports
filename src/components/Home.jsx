1010885d9f84a7f85c1258b42af1aa54695e71db
calebchris000 <calebchris000@gmail.com>
2023-04-18 16:10:14 +0100

    Grid system is now independent to each page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,28 @@
-import '../styles/Home.css';
-import React, { useEffect, useRef, useState } from 'react';
+import "../styles/Home.css";
+import React, {
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import {
   faArrowLeft,
   faSearch,
   faVirusCovid,
   faArrowRight,
-} from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { v4 as uuidv4 } from 'uuid';
-import PropTypes from 'prop-types';
+} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {v4 as uuidv4} from "uuid";
+import PropTypes from "prop-types";
 
 import {
   useDispatch,
   useSelector,
-} from 'react-redux';
-import GetData from '../redux/Home/HomeAPI';
-import Component from './Details';
-import { setDetails } from '../redux/Home/HomeSlice';
+} from "react-redux";
+import GetData from "../redux/Home/HomeAPI";
+import Component from "./Details";
+import {setDetails} from "../redux/Home/HomeSlice";
 
-const Info = ({ confirmed, deaths }) => (
+const Info = ({confirmed, deaths}) => (
   <div className="Info">
     <h2 className="global">
       GLOBAL (2020 - PRESENT)
@@ -38,7 +42,7 @@ const Info = ({ confirmed, deaths }) => (
   </div>
 );
 
-const Section = ({ country, cases, index }) => {
+const Section = ({country, cases, index}) => {
   const dispatch = useDispatch();
   function handleClick() {
     dispatch(setDetails(index));
@@ -54,7 +58,9 @@ const Section = ({ country, cases, index }) => {
           <FontAwesomeIcon icon={faArrowRight} />
         </i>
       </i>
-      <p className="country-section-text">{country}</p>
+      <p className="country-section-text">
+        {country}
+      </p>
       <p className="cases">{cases}</p>
     </button>
   );
@@ -62,10 +68,11 @@ const Section = ({ country, cases, index }) => {
 
 const Home = () => {
   const dispatch = useDispatch();
-  const { data, state, details } = useSelector(
-    (store) => store.Home,
+  const {data, state, details} = useSelector(
+    (store) => store.Home
   );
-  const [searchData, setSearchData] = useState('');
+  const [searchData, setSearchData] =
+    useState("");
   const arrowBack = useRef(null);
 
   useEffect(() => {
@@ -76,49 +83,61 @@ const Home = () => {
 
   function handleArrowBack() {
     dispatch(setDetails(null));
-    arrowBack.current.style.display = 'none';
+    arrowBack.current.style.display = "none";
   }
 
   let content;
 
-  if (state === 'Success') {
-    content = data.rawData
-      .slice(0, 700)
-      .filter((x) => x.Country_Region.toLowerCase().includes(
-        searchData.toLowerCase(),
-      ))
-      .map((item, index) => (
-        <Section
-          key={uuidv4()}
-          index={index}
-          country={
-            item.Country_Region === 'US'
-              ? `US: ${item.Province_State}`
-              : item.Country_Region
-          }
-          cases={item.Confirmed}
-        />
-      ));
+  if (state === "Success") {
+    content = (
+      <div className="second-row">
+        {data.rawData
+          .slice(0, 700)
+          .filter((x) =>
+            x.Country_Region.toLowerCase().includes(
+              searchData.toLowerCase()
+            )
+          )
+          .map((item, index) => (
+            <Section
+              key={uuidv4()}
+              index={index}
+              country={
+                item.Country_Region === "US"
+                  ? `US: ${item.Province_State}`
+                  : item.Country_Region
+              }
+              cases={item.Confirmed}
+            />
+          ))}
+      </div>
+    );
   }
 
-  if (state === 'Success' && details !== null && arrowBack.current !== null) {
-    arrowBack.current.style.display = 'block';
+  if (
+    state === "Success" &&
+    details !== null &&
+    arrowBack.current !== null
+  ) {
+    arrowBack.current.style.display = "block";
     content = (
-      <Component
+      <div className="details-row">
+        <Component
         date={data.rawData[details].Last_Update}
         country={
           data.rawData[details].Country_Region
         }
         incident={parseFloat(
-          data.rawData[details].Incident_Rate,
+          data.rawData[details].Incident_Rate
         ).toFixed(4)}
         cases={data.rawData[details].Confirmed}
         deaths={data.rawData[details].Deaths}
         ratio={parseFloat(
           data.rawData[details]
-            .Case_Fatality_Ratio,
+            .Case_Fatality_Ratio
         ).toFixed(4)}
       />
+      </div>
     );
   }
   return (
@@ -142,7 +161,9 @@ const Home = () => {
           <input
             type="text"
             value={searchData}
-            onChange={(e) => setSearchData(e.target.value)}
+            onChange={(e) =>
+              setSearchData(e.target.value)
+            }
             placeholder="Search Country"
           />
           <i className="search-icon">
@@ -152,17 +173,17 @@ const Home = () => {
       </nav>
       <Info
         confirmed={
-          state === 'Success'
+          state === "Success"
             ? data.summaryStats.global.confirmed
             : 0
         }
         deaths={
-          state === 'Success'
+          state === "Success"
             ? data.summaryStats.global.deaths
             : 0
         }
       />
-      <div className="second-row">{content}</div>
+     {content}
     </div>
   );
 };
