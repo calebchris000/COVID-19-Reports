a800aa75cb6039f6c7bdab7a266977002939d250
calebchris000 <calebchris000@gmail.com>
2023-04-25 15:05:48 +0100

    Add loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
+import './styles/App.css';
+import { useSelector } from 'react-redux';
 import Home from './components/Home';
 
 function App() {
+  const { state } = useSelector((store) => store.Home);
+  if (state === 'Pending') {
+    return (
+      <div className="loader">
+        <div className="loader-square" />
+        <div className="loader-square" />
+        <div className="loader-square" />
+        <div className="loader-square" />
+        <div className="loader-square" />
+        <div className="loader-square" />
+        <div className="loader-square" />
+      </div>
+    );
+  }
+
   return <Home />;
 }
 
